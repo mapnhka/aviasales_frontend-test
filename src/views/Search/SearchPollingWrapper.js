e0e0ee80e558a31ec7d98ae8a4ad19dee8c68bc8
m.vorobyeva <mapnhka.work@gmail.com>
2019-12-09 22:10:30 +0300

    Retry after fetch searchId error

--- src/views/Search/SearchPollingWrapper.js
+++ src/views/Search/SearchPollingWrapper.js
@@ -5,46 +5,59 @@ import {fetchSearchId} from '../../actions/client.actions';
 import {fetchTickets} from '../../actions/tickets.actions';
 
 export const withSearchPolling = (reconnectAttemptsNumber = 3, attemptDuration = 200) => ChildComponent => {
+
     const Wrapper = () => (
+
         class extends Component {
+
             componentDidMount() {
-                this.props.actions.fetchSearchId()
-                    .then((response) => {
-                        let reconnectAttemptsCounter = 0;
-                        const searchId = response.value.data.searchId;
-                        const subscribe = (searchId) => {
-                            this.props.actions.fetchTickets(searchId)
-                                .then((response) => {
-                                    reconnectAttemptsCounter = 0;
-                                    if (!response.value.data.stop) {
-                                        subscribe(searchId);
-                                    }
-                                })
-                                .catch((data) => {
-                                    if (reconnectAttemptsCounter < reconnectAttemptsNumber) {
-                                        reconnectAttemptsCounter++;
-                                        setTimeout(() => {
-                                            subscribe(searchId)
-                                        }, attemptDuration);
-                                    } else {
-                                        //TODO handle error
-                                        console.error("Error load tickets");
-                                    }
-                                });
-                        };
+                let reconnectAttemptsCounter = 0;
 
-                        subscribe(searchId);
+                const startSearch = () => {
+                    this.props.actions.fetchSearchId()
+                        .then((response) => {
+                            let reconnectAttemptsCounter = 0;
+                            const searchId = response.value.data.searchId;
 
-                        /*   this.props.actions.fetchTickets(searchId);
-                           this.dataPolling = setInterval(
-                               () => {
-                                   this.props.actions.fetchTickets(searchId);
-                               },
-                               duration);*/
-                    })
-                    .catch((error) => {
-                        //todo
-                    });
+                            const subscribe = (searchId) => {
+                                this.props.actions.fetchTickets(searchId)
+                                    .then((response) => {
+                                        reconnectAttemptsCounter = 0;
+
+                                        if (!response.value.data.stop) {
+                                            subscribe(searchId);
+                                        }
+                                    })
+                                    .catch((data) => {
+                                        if (reconnectAttemptsCounter < reconnectAttemptsNumber) {
+                                            reconnectAttemptsCounter++;
+
+                                            setTimeout(() => {
+                                                subscribe(searchId)
+                                            }, attemptDuration);
+                                        } else {
+                                            //TODO handle error
+                                            console.error("Error fetch tickets");
+                                        }
+                                    });
+                            };
+                            subscribe(searchId);
+                        })
+                        .catch((error) => {
+                            if (reconnectAttemptsCounter < reconnectAttemptsNumber) {
+                                reconnectAttemptsCounter++;
+
+                                setTimeout(() => {
+                                    startSearch();
+                                }, attemptDuration);
+                            } else {
+                                //TODO handle error
+                                console.error("Error fetch searchId");
+                            }
+                        });
+                };
+
+                startSearch();
             }
 
             componentWillUnmount() {
